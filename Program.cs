3f5bab95b694fac32eeca0bdaa3541972e6672fc
Xefise <xefise@gmail.com>
2022-08-25 16:12:12 +0300

    Inject DSBot config

--- Program.cs
+++ Program.cs
@@ -24,42 +24,19 @@ public class Program
     async Task MainAsync()
     {
         CreateHostBuilder().Build().Run();
-
-        /*
-        _client = _provider.GetRequiredService<DiscordSocketClient>();
-        _provider.GetRequiredService<CommandHandlerService>();
-
-        _client.Log += (LogMessage msg) =>
-        {
-            Console.WriteLine(msg.ToString());
-            return Task.CompletedTask;
-        };
-
-        var token = Environment.GetEnvironmentVariable("DSentBotToken");
-        await _client.LoginAsync(TokenType.Bot, token);
-        await _client.StartAsync();
-
-        _client.Ready += () =>
-        {
-            Console.WriteLine("Bot is connected!");
-            return Task.CompletedTask;
-        };
-
-        await Task.Delay(-1); // Block this task until the program is closed.
-        */
     }
 
-    static DiscordSocketConfig dsconfig = new()
+    public static DiscordSocketConfig dsconfig = new()
     {
-        LogLevel = LogSeverity.Debug
+        LogLevel = LogSeverity.Info
     };
+
     public static IHostBuilder CreateHostBuilder() =>
         Host.CreateDefaultBuilder()
             .ConfigureLogging(l => l.AddDebug().SetMinimumLevel(LogLevel.Debug))
             .ConfigureServices(services =>
             {
-                services.AddSingleton(dsconfig);
-                services.AddSingleton<DiscordSocketClient>();
+                services.AddSingleton<DiscordSocketClient>(new DiscordSocketClient(dsconfig));
                 services.AddSingleton<CommandService>();
                 services.AddHostedService<DiscordHostedService>();
                 services.AddHostedService<CommandHandlerService>();
