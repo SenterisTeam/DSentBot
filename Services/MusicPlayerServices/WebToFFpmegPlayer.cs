08af9eeacfc7622093b98495577376b1f45ab825
Xefise <xefise@gmail.com>
2022-09-03 00:20:12 +0300

    :satisfied: Fix packet loss

--- Services/MusicPlayerServices/WebToFFpmegPlayer.cs
+++ Services/MusicPlayerServices/WebToFFpmegPlayer.cs
@@ -12,7 +12,7 @@ public class WebToFFpmegPlayer: IMusicPlayer
         using (var stream = client.GetStreamAsync(music.Path))
         using (var ffmpeg = CreateStream(stream.Result))
         using (var output = ffmpeg.StandardOutput.BaseStream)
-        using (var discord = audioClient.CreatePCMStream(AudioApplication.Mixed, bufferMillis: 10000))
+        using (var discord = audioClient.CreatePCMStream(AudioApplication.Mixed, bufferMillis: 3000, packetLoss: 0))
         {
             try { await output.CopyToAsync(discord); }
             finally { await discord.FlushAsync(); }
@@ -24,7 +24,7 @@ public class WebToFFpmegPlayer: IMusicPlayer
         var process = Process.Start(new ProcessStartInfo
         {
             FileName = "ffmpeg",
-            Arguments = $"-hide_banner -loglevel panic -i - -ac 2 -f s16le -ar 48000 pipe:1",
+            Arguments = $"-hide_banner -loglevel debug -i - -ac 2 -f s16le -ar 48000 pipe:1", // #TODO change bitrate to 128k/256k
             UseShellExecute = false,
             RedirectStandardOutput = true,
             RedirectStandardInput = true
