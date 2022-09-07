22b306c51bfbd21b4e1299f5c7b03cf86e8fe764
Xefise <xefise@gmail.com>
2022-09-08 00:56:54 +0300

    Add help cmd

--- Services/DIscordBot/Modules/InfoModule.cs
+++ Services/DIscordBot/Modules/InfoModule.cs
@@ -17,4 +17,18 @@ public class InfoModule : ModuleBase<SocketCommandContext>
         });
 
     }
+
+    [Command ("help")]
+    public async Task HelpAsync()
+    {
+        EmbedBuilder embed = new EmbedBuilder{
+            Title = "Command list:",
+            Description = "Other information you can get [there.](https://github.com/SenterisTeam/DSentBot)"
+        };
+        embed.WithFooter(footer => footer.Text = "By Senteris Team.");
+        embed.AddField("Info", "~ping \n~help", false);
+        embed.AddField("Music", "~play *youtube music url* \n~stop", false);
+        embed.WithColor(Color.Gold);
+        await ReplyAsync(embed: embed.Build());
+    }
 }
